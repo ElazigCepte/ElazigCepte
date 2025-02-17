2e1b92f1985fe4e8a727478168ba9837f28cd4c6
Elazig Cepte <elazigcepte@gmail.com>
2025-02-17 15:48:00 +0300

    Add files via upload

---
+++ lib/hastaneler.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+
+class hastaneler extends StatelessWidget {
+  Widget build(BuildContext context) {
+    void hastane1 (context,Widget sayfa)
+    {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => sayfa));
+    }
+    return Scaffold(
+        appBar: AppBar(title: Text("Hastaneler", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),),
+        body: ListView(
+          children: [
+           Text("felfena ")
+          ],
+        )
+    );
+  }
+  Flexible butonOlustur({required String text, required VoidCallback fonksiyon , required width , required height}){
+    int buttonColor= 0xFF800020;
+    int textColor= (0xFFFFFFFF) ;
+    return Flexible(
+        child: ElevatedButton(
+          onPressed: fonksiyon,
+          child: Center(
+              child:Text(text)
+          ),
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(),
+              minimumSize: Size(width.toDouble(), height.toDouble()),
+              backgroundColor: Color(buttonColor),
+              foregroundColor: Color(textColor)
+          ),
+        )
+    );
+  }
+}
